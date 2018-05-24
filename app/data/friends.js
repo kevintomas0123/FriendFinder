5bbd07035971b850f130a7fa15ab8e615573f606
Kevin Tomas <kevin.tomas0123@gmail.com>
2018-05-24 15:08:52 -0700

    Finally done!

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+
+// Templated Friends Data
+var friendsData = [
+    {
+      name: "Izuku Midoriya",
+      photo: "https://i0.wp.com/animehound.com/wp-content/uploads/2017/11/Midoriya-Izuku-full-crowling.png?resize=560%2C396",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Katsuki Bakugo",
+      photo:"http://www.behindthevoiceactors.com/_img/chars/katsuki-bakugo--0.73.jpg",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "All Might",
+      photo:"https://img00.deviantart.net/97dd/i/2017/180/d/f/all_might_boku_no_hero_academia_by_glaucosilva-dbegdz8.jpg",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Ochaco Uraraka",
+      photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSl_ihhvRuOXbaPt44PJttVsfiDvPf-K9W3g6NfiCKwEGrROXbzBg",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Tenya Iida",
+      photo:"http://www.behindthevoiceactors.com/_img/chars/tenya-iida--7.51.jpg",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Shoto Todoroki",
+      photo:"https://ih0.redbubble.net/image.548381610.8144/flat,550x550,075,f.jpg",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Momo Yaoyorozu",
+      photo:"http://pm1.narvii.com/6567/3330515c31fb5993841a8379ef07d35a8d631065_00.jpg",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
